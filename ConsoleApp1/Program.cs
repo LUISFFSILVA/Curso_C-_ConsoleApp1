eee5da7dc04b88b599be2c78ac910656be30cfb2
@ssbuif <luis.silva3@scania.com>
2022-08-16 16:09:51 -0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var fisrtName = "Joao";
+            string lastName = "Silva";                        
+            //comentario
+            Console.WriteLine("\n\n" + "Qual é o seu nome?" + "\n");
+            Console.WriteLine("Meu nome é {0} {1}",fisrtName, lastName + "\n");
+
+            int idade = 19;
+            Console.WriteLine("\n\n" + "Qual é a sua idade?" + "\n");
+            Console.WriteLine("Minha idade é {0}", idade + "\n");
+
+            Console.ReadLine();
+        }
+    }
+}
